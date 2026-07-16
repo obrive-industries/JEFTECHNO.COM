eb3ade8ea90e27b4633b3d13e6fbf9d4588cb2ab
shivansh-006 <shivansh@obrive.com>
2026-07-16 21:57:17 +0530

    + fix arrow

--- src/components/navbar/MobileMenu.js
+++ src/components/navbar/MobileMenu.js
@@ -101,7 +101,7 @@ const MobileMenu = () => {
                 {item.label}
               </span>
               <img
-                src="/AboutUs/DropDownArr.png"
+                src="/AboutUs/DropdownArr.png"
                 alt="→"
                 className="w-4 h-4 object-contain -rotate-90"
               />
